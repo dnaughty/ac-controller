122026a6490d8a2c9aec4beeb20b97b332e9d55b
Denis Naughton <mjolinor75@hotmail.com>
2021-04-17 17:21:09 -0400

    finished video 1

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,16 +1,29 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-  	console.log("Connected to room channel...")
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbolinks:load', () => {
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+	const element = document.getElementById('room-id');
+	const room_id = element.getAttribute('data-room-id');
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+	// console.log(room_id)
+
+	consumer.subscriptions.create( { channel: "RoomChannel", room_id: room_id}, 
+	{
+	  connected() {
+	  	console.log("Connected to room channel " + room_id)
+	    // Called when the subscription is ready for use on the server
+	  },
+
+	  disconnected() {
+	    // Called when the subscription has been terminated by the server
+	  },
+
+	  received(data) {
+	    // Called when there's incoming data on the websocket for this channel
+	    console.log(data)
+	  }
+	});
+
+})
+
+
